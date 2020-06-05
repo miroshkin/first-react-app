f2cfc99c7598f656900c921a55f66c2ec41c45c5
miroshkin <anton.miroshkin@gmail.com>
2020-06-05 12:05:28 +0300

    Added header component

--- src/index.js
+++ src/index.js
@@ -4,20 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const title = '\'s first react element';
-const titleId = 'main-title';
-const desc = 'I just learned new React feature';
-const name = 'Anton'
+function Header () {
+  return (
+    <header>
+      <h1>Title</h1>
+      <span className="stats">Players:1</span>
+    </header>
+  );
+}
 
-const header = (
-  <header>
-    <h1 id = { titleId }>{ name } { title }</h1>
-    <p className='prgrph'>{ desc } {1 * 7}</p>
-    {/* <p>{ desc } {1 * 7}</p> */}
-  </header>
-);
-
-console.log(title);
 
 ReactDOM.render(
   header,
