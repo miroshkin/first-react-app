10e9583675939a0b58a8720be2859872a3309d33
miroshkin <anton.miroshkin@gmail.com>
2020-06-05 15:02:34 +0300

    Added counter component

--- src/index.js
+++ src/index.js
@@ -17,18 +17,33 @@ const Player = () => {
   return (
     <div className="player">
       <span className  ="player-name">Anton</span>
+      <Counter/>
+    </div>
+  );
+}
+
+const Counter = () => {
+  return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
         <span className="counter-score">35</span>
         <button className="counter-action increment"> + </button>
       </div>
+  );
+}
+
+const MyApp = () => {
+  return (
+    <div className="scoreboard">
+      <Header/>
+      {/* Players list */}
+      <Player/>
     </div>
   );
 }
 
-
 ReactDOM.render(
-  <Player />,
+  <MyApp />,
   document.getElementById('root')
 );
 
