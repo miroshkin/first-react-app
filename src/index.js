15afc68e90a28fe38667089d312c56dba78e1e68
miroshkin <anton.miroshkin@gmail.com>
2020-06-09 07:57:35 +0300

    Added props key

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ import * as serviceWorker from './serviceWorker';
 const players = [
   {
     name: "Guil",
-    score: 50
+    score: 50,
+    id: 1
   },
   {
     name: "Treasure",
-    score: 85
+    score: 85,
+    id: 2
   },
   {
     name: "Ashley",
-    score: 95
+    score: 95,
+    id: 3
   },
   {
     name: "James",
-    score: 80
+    score: 80,
+    id: 4
   }
 ];
 
@@ -58,7 +62,7 @@ const MyApp = (props) => {
       
       {/* Players list */}
       {props.initialPlayers.map( player =>
-        <Player playerName = {player.name} score={player.score} />
+        <Player playerName = {player.name} score={player.score} key={player.id.toString()} />
       )}
       
     </div>
